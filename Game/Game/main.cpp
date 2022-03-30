bf48f4f1d2b9f2f29672c4d7d838762e92e6cec4
ArabNinja <bartkowski_so@interia.pl>
2022-03-30 13:11:36 +0200

    usuwam test

--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -1,6 +1,18 @@
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-	
+	int width = 600;
+	int height = 400;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("PUTDungeon v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+
 	return 0;
 }
--- test.cpp
+++
@@ -1,8 +0,0 @@
-int main() {
-cout << "hi";
-}
-
-
-
-
-//Edited
