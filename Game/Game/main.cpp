21949ef6d2d1e6ad2c505471409fc83cc10f4166
ArabNinja <bartkowski_so@interia.pl>
2022-03-30 15:15:20 +0200

    Tworzy okienko

---
+++ Game/Game/main.cpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	int width = 800;
+	int height = 600;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("PUTDungeon v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 100, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+
+	return 0;
+}
